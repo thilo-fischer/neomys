4e51cc36881431a53bb35d37ac4f295a7b2a0e42
Thilo Fischer <thilo-fischer@gmx.de>
2015-02-04 15:02:35 +0100

    fix compile errors

--- neomys.c
+++ neomys.c
@@ -11,6 +11,14 @@
 #include <util/delay.h>
 #include "usb_keyboard.h"
 
+// FIXME why are these symbols not known from iom32u4 ?!?
+#define DDRB _SFR_IO8(0x04)
+#define PORTB _SFR_IO8(0x05)
+#define PINB _SFR_IO8(0x03)
+#define DDRF _SFR_IO8(0x10)
+#define PORTF _SFR_IO8(0x11)
+#define PINF _SFR_IO8(0x0F)
+
 // Data structures for addressing rows
 
 enum row_e {
@@ -23,48 +31,49 @@ enum row_e {
     ROW_COUNT
 };
 
-const int ROW_PORT_DDR [ROW_COUNT] = {  DDRF,  DDRF,  DDRF,  DDRF,  DDRF,  DDRF };
-const int ROW_PORT_PORT[ROW_COUNT] = { PORTF, PORTF, PORTF, PORTF, PORTF, PORTF };
-const int ROW_PORT_PIN [ROW_COUNT] = {  PINF,  PINF,  PINF,  PINF,  PINF,  PINF };
-const int ROW_PORT_BIT [ROW_COUNT] = {     0,     1,     4,     5,     6,     7 };
+
+volatile uint8_t * const ROW_PORT_DDR [ROW_COUNT] = { & DDRF, & DDRF, & DDRF, & DDRF, & DDRF, & DDRF };
+volatile uint8_t * const ROW_PORT_PORT[ROW_COUNT] = { &PORTF, &PORTF, &PORTF, &PORTF, &PORTF, &PORTF };
+volatile uint8_t * const ROW_PORT_PIN [ROW_COUNT] = { & PINF, & PINF, & PINF, & PINF, & PINF, & PINF };
+const    uint8_t         ROW_PORT_BIT [ROW_COUNT] = {      0,      1,      4,      5,      6,      7 };
 
 // Functions for addressing rows
 
 inline void init_row(uint8_t row) {
     // set up "open collector"
-    ROW_PORT_DDR [row] &= ~(1 << ROW_PORT_BIT[row]);
-    ROW_PORT_PORT[row] &= ~(1 << ROW_PORT_BIT[row]);
+    *ROW_PORT_DDR [row] &= ~(1 << ROW_PORT_BIT[row]);
+    *ROW_PORT_PORT[row] &= ~(1 << ROW_PORT_BIT[row]);
 }
 
 inline void activate_row(uint8_t row) {
     // output low
-    ROW_PORT_DDR [row] |=  (1 << ROW_PORT_BIT[row]);
+    *ROW_PORT_DDR [row] |=  (1 << ROW_PORT_BIT[row]);
 }
 
 inline void deactivate_row(uint8_t row) {
     // input (open collector)
-    ROW_PORT_DDR [row] &= ~(1 << ROW_PORT_BIT[row]);
+    *ROW_PORT_DDR [row] &= ~(1 << ROW_PORT_BIT[row]);
 }
 
 // Data structures for addressing columns
 
 #define COL_COUNT 8
 
-const int COL_PORT_DDR [COL_COUNT] = {  DDRB,  DDRB,  DDRB,  DDRB,  DDRB,  DDRB,  DDRB,  DDRB };
-const int COL_PORT_PORT[COL_COUNT] = { PORTB, PORTB, PORTB, PORTB, PORTB, PORTB, PORTB, PORTB };
-const int COL_PORT_PIN [COL_COUNT] = {  PINB,  PINB,  PINB,  PINB,  PINB,  PINB,  PINB,  PINB };
-const int COL_PORT_BIT [COL_COUNT] = {     0,     1,     2,     3,     4,     5,     6,     7 };
+volatile uint8_t * const COL_PORT_DDR [COL_COUNT] = { & DDRB, & DDRB, & DDRB, & DDRB, & DDRB, & DDRB, & DDRB, & DDRB };
+volatile uint8_t * const COL_PORT_PORT[COL_COUNT] = { &PORTB, &PORTB, &PORTB, &PORTB, &PORTB, &PORTB, &PORTB, &PORTB };
+volatile uint8_t * const COL_PORT_PIN [COL_COUNT] = { & PINB, & PINB, & PINB, & PINB, & PINB, & PINB, & PINB, & PINB };
+const    uint8_t         COL_PORT_BIT [COL_COUNT] = {      0,      1,      2,      3,      4,      5,      6,      7 };
 
 // Functions for addressing cols
 
 inline void init_col(uint8_t col) {
     // set up input with pull up
-    COL_PORT_DDR [col] &= ~(1 << COL_PORT_BIT[col]);
-    COL_PORT_PORT[col] &=  (1 << COL_PORT_BIT[col]);
+    *COL_PORT_DDR [col] &= ~(1 << COL_PORT_BIT[col]);
+    *COL_PORT_PORT[col] &=  (1 << COL_PORT_BIT[col]);
 }
 
 inline char test_col(uint8_t col) {
-    return (COL_PORT_PIN[col] &  (1 << COL_PORT_BIT[col])) ? 1 : 0 ;
+    return (*COL_PORT_PIN[col] &  (1 << COL_PORT_BIT[col])) ? 1 : 0 ;
 }
 
 // global symbols
@@ -218,6 +227,12 @@ char prev_row_state[2][ROW_COUNT];
 #  define CONTROLLER CTLR_SLAVE
 #endif
 
+void process_states() {
+    if (memcmp(prev_row_state, row_state, 2*ROW_SPACE*sizeof(row_state[0][0]))) {
+        memcpy(prev_row_state, row_state, 2*ROW_SPACE*sizeof(row_state[0][0]));
+    }
+}
+
 int main(void) {
     uint8_t row;
     for (row = 0; row < ROW_COUNT; ++row) {
@@ -230,16 +245,10 @@ int main(void) {
         deactivate_row(row);
     }
 #if (CONTROLLER == CTLR_MASTER)
-    rx_slave(row_state[CTLR_SLAVE]);
+    //    rx_slave(row_state[CTLR_SLAVE]);
     process_states();
 #else
     tx_states(row_state[CONTROLLER]);
 #endif
     return 0;
-}
-
-void process_states() {
-    if (memcmp(prev_row_state, row_state, 2*ROW_SPACE*sizeof(row_state[0][0]))) {
-        memcpy(prev_row_state, row_state, 2*ROW_SPACE*sizeof(row_state[0][0]));
-    }
 }
