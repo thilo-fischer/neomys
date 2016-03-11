4de81b8aa26ac346052b07b8cba8078a49b8037e
Thilo Fischer <thilo.fischer@ksti.de>
2016-03-11 14:17:11 +0100

    ...

--- src/debug.c
+++ src/debug.c
@@ -9,136 +9,177 @@
  * Generic parts and inclusion of specific debug implementation.
  */
 
+//////////////////
+// declarations //
+//////////////////
+
+/// Output debug message to all currently active channels.
+static void dbg_vprocess(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, const va_list start_argptr);
+
+/// Output debug message to all channels active in the given bitfield.
+static void dbg_vprocess(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, const va_list start_argptr, dbg_channel_spec_t dest_channels);
+
+
+/////////////////
+// definitions //
+/////////////////
 
 enum dbg_level_e dbg_threshold = DBG_WARN;
 
-uint8_t dbg_active_channels = DBG_CH_BUFFER;
+dbg_channel_spec_t dbg_active_channels = DBG_CH_BUFFER;
 
+/// whether to send just error codes via UART (in contrast to sending
+/// string corstructed form the printf-alike format string and
+/// optional additional arguments)
+bool dbg_uart_code = true;
+/// when dbg_uart_code is active: send binary data of additional
+/// arguments
+bool dbg_uart_payload = true;
 
+bool dbg_uart_add_prefix = false; ///< prepend a certain byte pattern to UART debug output
+char dbg_uart_prefix = '\0'; ///< byte to prepend to UART debug output
 
-bool dbg_uart_code = true; ///< whether to send just error codes via
-			   ///  UART (in contrast to sending string
-			   ///  corstructed form the printf-alike
-			   ///  format string and optional additional
-			   ///  arguments)
-bool dbg_uart_payload = true; ///< when dbg_uart_code is active: send
-			      ///  binary data of additional arguments
+bool dbg_uart_add_suffix = false; ///< append a certain byte pattern to UART debug output
+char dbg_uart_suffix = '\0'; ///< byte to append to UART debug output
 
-bool dbg_uart_add_prefix = false; ///< prepend a certain byte pattern to the debug output
-char dbg_uart_prefix = '\0';
-
-bool dbg_uart_add_suffix = false; ///< append a certain byte pattern to the debug output
-char dbg_uart_suffix = '\0';
-
-
-
+/// maximum size of debug output in binary format
 static const uint16_t dbg_max_out_binary_size =  12;
+/// maximum size of debug output in text format
 static const uint16_t dbg_max_out_string_size = 128;
 
-static void dbg_vprocess(enum dbg_level_e lvl, uint8_t errcode, char *format, arg_sizes[], arg_sizes_cnt, const va_list start_argptr);
+////////////////////
+// implementation //
+////////////////////
 
-void dbg_set_threshold(enum dbg_level_e t) {
-  threshold = t;
-}
-
-
-void dbg_process(enum dbg_level_e lvl, uint8_t errcode, char *format, arg_sizes[], arg_sizes_cnt, ...) {
+void dbg_process(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, ...) {
   va_list argptr;
   va_start(argptr, format);
-  dbg_process(lvl, errcode, format, argsizes, arg_sizes_cnt, argptr);
+  dbg_process(lvl, msgspec, argptr);
   va_end(argptr);
 }
 
-static void dbg_vprocess(enum dbg_level_e lvl, uint8_t errcode, char *format, arg_sizes[], arg_sizes_cnt, const va_list start_argptr) {
+static void dbg_vprocess(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, const va_list start_argptr) {
+  dbg_voutput(lvl, msgspec, start_argptr, dbg_active_channels);
+}
 
-  if (dbg_is_binary_active()) {
+static void dbg_voutput(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, const va_list start_argptr, dbg_channel_spec_t dest_channels) {
+
+  const uint8_t errcode = msgspec->errcode;
+  const char *const format = msgspec->format;
+  const uint8_t *arg_sizes = msgspec->arg_sizes;
+  const uint8_t args_cnt = msgspec->args_cnt;
+
+  if (dbg_is_binary_active(dest_channels)) {
     char bin_buf[dbg_max_out_binary_size];
-    uint8_t bin_buf_cnt = 0;
+    uint8_t bin_buf_size = 0;
     
     va_list argptr;
     va_copy(argptr, start_argptr);
     
-    for (uint8_t i = 0; i < arg_sizes_cnt; ++i) {
+    for (uint8_t i = 0; i < args_cnt; ++i) {
 
-      if (bin_buf_cnt + arg_sizes[i] >= sizeof(bin_buf)) {
+      if (bin_buf_size + arg_sizes[i] >= sizeof(bin_buf)) {
+	// TODO signal error state?
 	break;
       }
 
       switch (arg_sizes[i]) {
       case 1:
-	(uint8_t *) &(bin_buf[bin_buf_cnt]) = va_arg(argptr, uint8_t);
+	(uint8_t *) &(bin_buf[bin_buf_size]) = va_arg(argptr, uint8_t);
 	break;
       case 2:
-	(uint16_t*) &(bin_buf[bin_buf_cnt]) = va_arg(argptr, uint16_t);
+	(uint16_t*) &(bin_buf[bin_buf_size]) = va_arg(argptr, uint16_t);
 	break;
       case 4:
-	(uint32_t*) &(bin_buf[bin_buf_cnt]) = va_arg(argptr, uint32_t);
+	(uint32_t*) &(bin_buf[bin_buf_size]) = va_arg(argptr, uint32_t);
 	break;
       default:
 	// TODO signal error state?
-	memset(&(bin_buf[bin_buf_cnt]), 0x00, arg_sizes[i])
-      }
+	memset(&(bin_buf[bin_buf_size]), 0x00, arg_sizes[i]);
+      } // switch
       
-      bin_buf_cnt += arg_sizes[i];
+      bin_buf_size += arg_sizes[i];
+    } // for
+
+
+    if (dbg_active_channels | DBG_CH_BUFFER && to_buffer) {
+      dbg_buf_add(lvl, msgspec, bin_buf, bin_buf_size);
     }
 
-
-    if (dbg_active_channels | DBG_CH_BUFFER) {
-      dbg_buf_add(lvl, errcode, bin_buf, bin_buf_cnt);
-    }
-
-    
     if ((dbg_active_channels | DBG_CH_UART) && (dbg_uart_code)) {
-      if (dbg_uart_add_prefix) {
-	uc_uart_send_char(dbg_uart_prefix);
-      }
-      uc_uart_send_char(errcode);
-      if (dbg_uart_payload) {
-	uc_uart_send_blob(bin_buf, bin_buf_cnt);
-      }
-      if (dbg_uart_add_suffix) {
-	buffer[buf_size++] = dbg_uart_suffix;
-      }
+      dbg_output_uart_bin(errcode, bin_buf, bin_buf_size);
     }
-  }
+  } // dbg_is_binary_active(dest_channels)
 
   if (dbg_is_string_active()) {
-    char str_buf[dbg_max_out_string_size];
-    
-    va_list argptr;
-    va_copy(argptr, start_argptr);
-
-    int strlen = vsnprintf(str_buf, dbg_max_out_string_size, format, argptr);
-
-    if (dbg_active_channels | DBG_CH_KEYS) {
-      hc_enq_syms_from_str(str_buf, strlen);
-    }
-
-    if ((dbg_active_channels | DBG_CH_UART) && (!dbg_uart_code)) {
-      uc_uart_send_blob(str_buf, strlen);      
-    }
+    dbg_output_string(lvl, msgspec, start_argptr);
   }
   
 }
 
 
-void dbg_output_buffer() {
+void dbg_output_uart_bin(uint8_t errcode, char *bin_buf, uint8_t bin_buf_size) {
+  if (dbg_uart_add_prefix) {
+    uc_uart_send_char(dbg_uart_prefix);
+  }
+  uc_uart_send_char(errcode);
+  if (dbg_uart_payload) {
+    uc_uart_send_blob(bin_buf, bin_buf_size);
+  }
+  if (dbg_uart_add_suffix) {
+    buffer[buf_size++] = dbg_uart_suffix;
+  }
+}
+
+void dbg_output_string(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, va_list argptr) {
+  char str_buf[dbg_max_out_string_size];
+  int strlen = vsnprintf(str_buf, dbg_max_out_string_size, format, argptr);
+  
+  if (dbg_active_channels | DBG_CH_KEYS) {
+    hc_enq_syms_from_str(str_buf, strlen);
+  }
+  
+  if ((dbg_active_channels | DBG_CH_UART) && (!dbg_uart_code)) {
+    uc_uart_send_blob(str_buf, strlen);      
+  }
+}
+
+#if 0 // TODO
+
+uint32_t dbg_buffer_ovfl = 0;
+dbg_define_msg(dbg_buffer_ovfl, 0xFF, "debug msg buffer overflown by %i bytes", sizeof(dbg_buffer_ovfl))
+
+void dbg_buf_add(lvl, struct dbg_msgspec_s *msg, char *bin_buf, uint8_t bin_buf_size) {
+  dbg_buf_push_char(lvl);
+  dbg_buf_push_ptr(msg);
+  dbg_buf_push_blob(bin_buf, bin_buf_size);
+}
+
+void dbg_flush_buffer(dbg_channel_spec_t dest_channels) {
+  if (dbg_buffer_ovfl) {
+    dbg_warn(dbg_buffer_ovfl, dbg_buffer_ovfl)
+  }
   while (!dbg_buf_empty()) {
     uint8_t lvl = dbg_buf_pop_char();
-    uint8_t errcode = dbg_buf_pop_char();
+    struct dbg_msgspec_s *msg = dbg_buf_pop_ptr();
     uint8_t arg_cnt = dbg_buf_pop_char();
 
-    uint32_t args[dbg_max_arg_cnt];
-    void *bin_arg_ptr = dbg_buf_cursor();
+    // XXX creating function invokation arguments from binary data may
+    // have platform specific aspects (litte endian vs. big endian and
+    // such). => extract to a controller specific function located in
+    // ucontroller_*.h
+    uint32_t args[dbg_max_msg_arg_cnt];
+    uint8_t *bin_arg_ptr = dbg_buf_cursor();
     
-    for (uint8_t i = 0; i < arg_cnt; ++i) {
+    for (uint8_t arg = 0; arg < arg_cnt; ++arg) {
+      for (uint8_t byte = 0; byte < msg->args_cnt; )
       args[i] = dbg_buf_pop_char();
     }
     dbg_output(
 	       lvl, errcode, format,
 	       bin_arg_ptr,
 	       bin_arg_size,
-	       args[0], args[1], args[2], args[3], args[4], args[5], args[6]
+	       args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
 	       );
     
     //uint32_t a01, a02, a03, a04, a05, a06;
@@ -151,3 +192,9 @@ void dbg_output_buffer() {
     //}
   }
 }
+
+#else
+
+#define dbg_buf_add(...)
+
+#endif
--- src/debug.h
+++ src/debug.h
@@ -13,68 +13,175 @@
  */
 
 
+#ifdef NDEBUG
+
+#define dbg_init(...)
+#define dbg_cycle(...)
+#define dbg_set_threshold(...)
+
+#define dbg_define_msg(...)
+
+#define dbg_error(...)
+#define dbg_warn(...)
+#define dbg_info(...)
+
+#define dbg_flush_buffer(...)
+
+#else // def NDEBUG
+// !def(NDEBUG)
+
 void dbg_init();
 
 void dbg_cycle();
 
+/// Debug messages shall be categorized with a loglevel indicating the
+/// severity of the issue being logged. The enum defines the supported
+/// loglevels.
 enum dbg_level_e {
-  DBG_LVL_NONE,
-  DBG_LVL_ERROR,
-  DBG_LVL_WARN,
-  DBG_LVL_INFO,
+  DBG_LVL_INFO, ///< informative messages
+  DBG_LVL_WARN, ///< messages to signal something not working as expected
+  DBG_LVL_ERROR, ///< messages to signal something is going seriously wrong
+  DBG_LVL_ENUM_END ///< not a loglevel, just a reference behind the last enum element
 };
+/// Only debug messages with a loglevel equal to or greater than the
+/// value configured as threshold will be processed. Set to
+/// DBG_LVL_ENUM_END to disable logging at all.
 extern enum dbg_level_e dbg_threshold;
 
+static inline void dbg_set_threshold(enum dbg_level_e t) {
+  dbg_threshold = t;
+}
+
+/// Debug messages can be sent via one or multiple interfaces to
+/// different destinations. The enum defines the set of supported
+/// channels and specifies bit positions to store the set of active
+/// channels in a bitfield.
 enum dbg_channels_e {
-  DBG_CH_BUFFER = (1 << 0),
-  DBG_CH_UART   = (1 << 1),
-  DBG_CH_KEYS   = (1 << 2),
+  DBG_CH_BUFFER = (1 << 0), ///< store all relevant data of the debug
+			    ///  message in a buffer and (optionally)
+			    ///  signal to the keyboard user there is
+			    ///  new data in the buffer.
+  DBG_CH_UART   = (1 << 1), ///< send debug info via a UART interface of the controller
+  DBG_CH_KEYS   = (1 << 2), ///< send keycodes to the host that will
+			    ///  make the debug message appear in a text
+			    ///  editor
 };
-extern uint8_t dbg_active_channels;
+/// Bitfield to specify the set of active channels. Debug messages
+/// will be directed only to those channels whose bit is high in this
+/// bitfield.
+typedef uint8_t dbg_channel_spec_t;
+extern dbg_channel_spec_t dbg_active_channels;
 
-#define dbg_declare_msg(id, errcode, format, ...)			\
-  const uint8_t dbg_msg_##id##_errcode = errcode;			\
-  const char *const dbg_msg_##id##_format = format;			\
-  const uint8_t dbg_msg_##id##_arg_sizes[] = __VA_ARGS__;
+/// format used to store info about defined debug messages. @see dbg_define_msg
+struct dbg_msgspec_s {
+  uint8_t errcode;    ///< The error code to be associated with this
+                      ///  debug message. To be used when sending
+                      ///  debug messages is an abbreviated binary
+                      ///  format.
+  char *const format; ///< Format string compatible to printf format
+                      ///  strings. To be used when outputting the
+                      ///  debug message in text form.
+  uint8_t *arg_sizes; ///< Byte sizes of additional arguments to be expected.
+  uint8_t args_cnt;   ///< Number of additional arguments to be expected.
+  uint8_t args_total_size; ///< Sum of byte sizes of all additional arguments.
+};
 
-//const uint8_t dbg_msg_##id##payload_size = payload_size;
-//struct dbg_msg_##id##_args_s { __VA_ARGS__ };				\
-//const uint16_t dbg_msg_##id##_args_size = sizeof(struct dbg_msg_##id##_args_s)
-
-/// For internal use, shuold not be called directly.
-/// Call dbg_error, dbg_warn or dbg_info instead.
-#define dbg_resolve_args(lvl, id, ...)			\
-  dbg_process(						\
-	      lvl,					\
-	      dbg_msg_##id##_errcode,			\
-	      dbg_msg_##id##_format,			\
-	      dbg_msg_##id##_arg_sizes,			\
-	      sizeof(dbg_msg_##id##_arg_sizes),		\
-	      __VA_ARGS__				\
-						)
-
-/// For internal use, shuold not be called directly.
-/// Call dbg_error, dbg_warn or dbg_info instead.
-#define dbg_add(lvl, ...) {					\
-    if (lvl <= dbg_threshold && dbg_is_active()) {		\
-      dbg_resolve_args(lvl, __VA_ARGS__);				\
-    }								\
+/// Debug system is not prepared to send arbitrary messages to the
+/// channels. All messages that possibly might be sent at runtime need
+/// to be declared somewhere in the code. These debug message
+/// definitions specify the associations of error codes and debug
+/// messages and also allow to store debug message data in the
+/// ringbuffer with minimal memory consumption. (The latter is very
+/// beneficial on small controllers with little RAM).
+/// 
+/// @param[in] id the identifier used to refer to the debug message
+/// being defined.
+///
+/// @param[in] errcode The error code to be associated with this debug
+/// message. May be a char or a signed or unsigned int8_t. Does not
+/// have any technical relevance. To be used when sending debug
+/// messages is an abbreviated binary format.
+///
+/// @param[in] Format Format string compatible to printf format
+/// strings. To be used when outputting the debug message in text form.
+/// 
+/// @param[in] ... If the format string expects additional arguments
+/// and/or additional binary data shall be appended to the error code
+/// when outputting the debug message in an abbreviated binary format,
+/// a list of the sizes (in bytes) of those data types of the expected
+/// additonal arguments shall be given as variable argument list.
+#define dbg_define_msg(id, errcode, format, ...)			\
+  static const uint8_t dbg_msg_##id##_arg_sizes[] = { __VA_ARGS__ };	\
+  const struct dbg_msgspec_s dbg_msg_##id = {				\
+    (uint8_t) errcode,							\
+    format,								\
+    &dbg_msg_##id##_arg_sizes,						\
+    sizeof(dbg_msg_##id##_arg_sizes),					\
+    util_sum_args(__VA_ARGS__)						\
   }
 
-#define dbg_error(...) {				\
+/// Output debug message with loglevel DBG_LVL_ERROR.
+/// 
+/// @param[in] ... First parameter shall be the identifier refering to
+/// the debug message to output. Additional arguments shall be given
+/// according to the debug message's expectation of additional
+/// arguments.
+#define dbg_error(...) {					\
     dbg_add(DBG_LVL_ERROR, __VA_ARGS__);			\
   }
-
-#define dbg_warn(...) {				\
+  
+/// Output debug message with loglevel DBG_LVL_WARN. For parameter
+/// desciption, see @see dbg_error.
+#define dbg_warn(...) {					\
     dbg_add(DBG_LVL_WARN, __VA_ARGS__);			\
   }
 
-#define dbg_info(...) {				\
+/// Output debug message with loglevel DBG_LVL_INFO. For parameter
+/// desciption, see @see dbg_error.
+#define dbg_info(...) {					\
     dbg_add(DBG_LVL_INFO, __VA_ARGS__);			\
   }
 
-static inline bool dbg_is_active() {
+/// For internal use, shuold not be called directly.
+/// Call dbg_error, dbg_warn or dbg_info instead.
+///
+/// Wrapper of dbg_proces to be referenced from dbg_error, dbg_warn
+/// and dbg_info.
+#define dbg_add(lvl, ...) {						\
+    if (lvl >= dbg_threshold && dbg_has_active_channels()) {		\
+      dbg_resolve_args(lvl, __VA_ARGS__);				\
+    }									\
+  }
+
+/// For internal use, shuold not be called directly.
+/// Call dbg_error, dbg_warn or dbg_info instead.
+///
+/// Wrapper of dbg_proces to be referenced from dbg_add. Extract debug
+/// message id from variable argument list.
+#define dbg_resolve_args(lvl, id, ...)				\
+  dbg_process(							\
+	      lvl,						\
+	      &dbg_msg_##id,					\
+	      __VA_ARGS__					\
+	     )
+
+/// For internal use.
+/// @return true if at least one channel is active, false otherwise.
+static inline bool dbg_has_active_channels() {
   return dbg_active_channels > 0;
 }
 
-void dbg_process(enum dbg_level_e lvl, uint8_t errcode, char *format, arg_sizes[], arg_sizes_cnt, ...);
+/// For internal use, shuold not be called directly.
+/// Call dbg_error, dbg_warn or dbg_info instead.
+///
+/// Output debug message to all currently active channels.
+void dbg_process(enum dbg_level_e lvl, struct dbg_msgspec_s *msgspec, ...);
+
+/// Output buffered dbg messages to the specified channels. Clear
+/// buffer afterwards (unless one of the specified channels is the
+/// buffer channel.)
+void dbg_flush_buffer(dbg_channel_spec_t dest_channels);
+
+
+#endif // def NDEBUG
+
